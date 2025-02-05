4ab5478295f38ead68af969ba07bb1e012c8ed3c
Asmita Sharma <155943217+asmitasharma04@users.noreply.github.com>
2025-02-05 13:04:47 +0530

    Add files via upload

---
+++ leetcode876.java
@@ -0,0 +1,40 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+}
+
+public class leetcode876 {
+    public static void main(String[] args) {
+        // Create a sample linked list: 1 -> 2 -> 3 -> 4 -> 5
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        Solution solution = new Solution(); // Correctly create an instance of Solution
+        ListNode middle = solution.middleNode(head);
+
+        // Print the value of the middle node
+        System.out.println("Middle node value: " + middle.val);
+    }
+}
